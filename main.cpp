56ce532632b95c89e637fc5d612bad6200e2864f
William Turbide Auclair <william.turbide-auclair.1@ulaval.ca>
2025-06-29 14:21:17 -0400

    feat: init projet et ajout affichage_matrice

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+========================================================================
+  Fichier     : main.cpp
+  Auteur      : William Turbide Auclair
+  Date        : 29 juin 2025
+  Description : Fichier main.cpp pour tester les exercises.
+  Version     : 1.0
+  ========================================================================
+*/
+#include <iostream>
+
+int main() {
+    std::cout << "Bonjour, monde !" << std::endl;
+    return 0;
+}
---
+++ src/affichage_matrice.cpp
@@ -0,0 +1,13 @@
+/*
+========================================================================
+  Fichier     : affichage_matrice.cpp
+  Auteur      : William Turbide Auclair
+  Date        : 29 juin 2025
+  Description : Affiche toute les possibilité d'une matrice dans la console.
+  Version     : 1.0
+  ========================================================================
+*/
+
+#include <iostream>
+
+
