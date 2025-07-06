febf6f47ab848044a0d2fd0194393ae50df32fa8
William Turbide Auclair <williamauclair22@gmail.com>
2025-07-06 17:43:42 -0400

    Link_List: deleteFirst(), deleteLast()

--- src/structure_donnee/link_list/Link_List.cpp
+++ src/structure_donnee/link_list/Link_List.cpp
@@ -24,7 +24,9 @@ link_list<T>::link_list(const link_list &other) {
 
 template<typename T>
 link_list<T>::~link_list() {
-
+    while (first != nullptr) {
+        deleteFirst();
+    }
 }
 
 template<typename T>
@@ -53,14 +55,36 @@ void link_list<T>::addLast(T value) {
 
 template<typename T>
 void link_list<T>::deleteFirst() {
+    if (first != nullptr) {
+        auto to_Delete = first;
+        first = to_Delete->next;
+
+        if (first == nullptr) {
+            last = nullptr;
+        }
+
+        delete to_Delete;
+    }
 }
 
 template<typename T>
 void link_list<T>::deleteLast() {
+    if (last != nullptr && first != last) {
+        auto to_Delete = last;
+        auto new_last = first;
+        while (new_last->next != last) {
+            new_last = new_last->next;
+        }
+        last = new_last;
+        new_last->next = nullptr;
+        delete to_Delete;
+    } else if (last != nullptr && first == last) {
+        deleteFirst();
+    }
 }
 
 template<typename T>
-bool link_list<T>::contains(const T& value) const {
+bool link_list<T>::contains(const T &value) const {
     return true;
 }
 
