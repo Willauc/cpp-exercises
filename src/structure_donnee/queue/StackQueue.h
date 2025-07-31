1a207172e84b8cc63d1f329f991d893722dd186b
William Turbide Auclair <william.turbide-auclair.1@ulaval.ca>
2025-07-31 10:45:38 -0400

    StackQueue : INIT + test

---
+++ src/structure_donnee/queue/StackQueue.h
@@ -0,0 +1,20 @@
+/*
+========================================================================
+  Fichier     : StackQueue.h
+  Auteur      : William Turbide Auclair
+  Date        : 31 juillet 2025
+  Description : Exercice du cours "The Ultimate Data Structures & Algorithms: Part 1" de Mosh.
+                Le but est d'implementer un Queue en utilisant deux Stack.
+
+  Version     : 1.0
+  ========================================================================
+*/
+#include "C:\Users\tzar_\OneDrive\Bureau\python-exercises\cpp-exercises\src\structure_donnee\stack\Stack.h"
+
+#ifndef STACKQUEUE_H
+#define STACKQUEUE_H
+template <typename T>
+class ArrayQueue {
+
+};
+#endif //STACKQUEUE_H
---
+++ tests/StackQueueTest.cpp
@@ -0,0 +1,17 @@
+/*
+========================================================================
+  Fichier     : StackQueueTest.cpp
+  Auteur      : William Turbide Auclair
+  Date        : 31 juillet 2025
+  Description : Exercice du cours "The Ultimate Data Structures & Algorithms: Part 1" de Mosh.
+                Le but est d'implementer un Queue en utilisant deux Stack. Fichier Test.
+
+  Version     : 1.0
+  ========================================================================
+*/
+#include <gtest/gtest.h>
+#include "structure_donnee/queue/StackQueue.h"
+
+TEST(StackQueueTest, constructeur) {
+
+}
